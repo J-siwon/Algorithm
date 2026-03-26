a056e2a07c5725e07b19f008c101e0f8d6658da3
Siwon Jeong <81245651+J-siwon@users.noreply.github.com>
2026-03-26 22:29:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 110064 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,14 @@
+import sys
+import math
+from collections import deque
+input = sys.stdin.readline
+
+x = int(input())
+dp = [0] * (x+1)
+dp[0] = 1
+dp[1] = 1
+for i in range(2,x+1):
+    dp[i] = dp[i-1] + dp[i-2]
+    dp[i] %= 10007
+
+print(dp[x])
